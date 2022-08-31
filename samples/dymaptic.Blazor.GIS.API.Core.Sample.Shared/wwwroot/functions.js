c706fd310426bd23811d7b4351387bebc3fed70d
Tim Purdum <tim@cedarrivertech.com>
2022-08-31 14:12:32 -0500

    fix scrollIntoView bug on FireFox

--- samples/dymaptic.Blazor.GIS.API.Core.Sample.Shared/wwwroot/functions.js
+++ samples/dymaptic.Blazor.GIS.API.Core.Sample.Shared/wwwroot/functions.js
@@ -1,7 +1,14 @@
 ﻿window.scrollToNav = (page) => {
     let navItems = document.getElementsByTagName('a');
     let navItem = Array.from(navItems).find(i => i.href === page);
-    navItem?.scrollIntoViewIfNeeded();
+    if (navigator.userAgent.indexOf('Firefox') === -1) {
+        // only do this when not in FireFox
+        navItem?.scrollIntoViewIfNeeded();
+    } else {
+        if (!elementIsVisible(navItem)) {
+            navItem.scrollIntoView();
+        }
+    }
 }
 
 window.loadApiKeyFromLocalStorage = () => {
@@ -10,4 +17,13 @@ window.loadApiKeyFromLocalStorage = () => {
 
 window.saveApiKeyToLocalStorage = (apiKey) => {
     window.localStorage['ArcGISApiKey'] = apiKey;
+}
+
+
+function elementIsVisible(item) {
+    
+    let eleTop = item.offsetTop;
+    let eleBottom = eleTop + item.clientHeight;
+
+    return (eleTop >=0 && eleBottom <= window.innerHeight);
 }
