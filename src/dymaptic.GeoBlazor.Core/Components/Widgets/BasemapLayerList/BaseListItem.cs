1837ebc99cd2e89a87a404b83636cb765502b67e
seahro <rawson.t@gmail.com>
2022-10-24 09:49:41 -0700

    initial basemap widget file component setup

---
+++ src/dymaptic.GeoBlazor.Core/Components/Widgets/BasemapLayerList/BaseListItem.cs
@@ -0,0 +1,22 @@
+﻿using dymaptic.GeoBlazor.Core.Components.Widgets.LayerList;
+using Microsoft.AspNetCore.Components;
+
+
+namespace dymaptic.GeoBlazor.Core.Components.Widgets.BasemapLayerList;
+
+public class BaseListItem
+{
+    [Parameter]
+    public string? BasemapTitle { get; set; }
+
+    [Parameter]
+    public List<ListItem>? BasemapItems { get; set; }
+
+    [Parameter]
+    public List<ListItem>? ReferenceItems { get; set; }
+
+    [Parameter]
+    public bool? Visible { get; set; }
+
+    public ActionBase[][]? ActionSections { get; set; }
+}
---
+++ src/dymaptic.GeoBlazor.Core/Components/Widgets/BasemapLayerListWidget.cs
@@ -0,0 +1,27 @@
+﻿using dymaptic.GeoBlazor.Core.Components.Widgets.LayerList;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Text.Json.Serialization;
+
+
+namespace dymaptic.GeoBlazor.Core.Components.Widgets;
+
+public class BasemapLayerListWidget : Widget
+{
+    [JsonPropertyName("type")]
+    public override string WidgetType => "basemapLayerList";
+    
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? IconClass { get; set; }
+    
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Label { get; set; }
+    
+    [Parameter]
+    [JsonIgnore]
+    public Func<ListItem, Task<ListItem>>? onListItemCreatedHandler { get; set; }
+    
+    public DotNetObjectReference<BasemapLayerListWidget> BasemapLayerListWidgetObjectReference => DotNetObjectReference.Create(this);
+}
