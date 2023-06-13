985dd8f8256e596400f1d08aff17cd4091a1f76f
Tim Rawson <rawson.t@gmail.com>
2023-06-12 20:22:18 -0700

    initial widget class setup

---
+++ src/dymaptic.GeoBlazor.Core/Components/Widgets/BookmarkWidget.cs
@@ -0,0 +1,42 @@
+﻿using dymaptic.GeoBlazor.Core.Components.Views;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace dymaptic.GeoBlazor.Core.Components.Widgets;
+/// <summary>
+/// The bookmarks widget displays a collection of bookmarks defined in the map or manually by the user. 
+/// The Bookmarks widget allows end users to quickly navigate to a particular area of interest. 
+/// It displays a list of bookmarks, which are typically defined inside the WebMap.
+/// <a target="_blank" href="https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Bookmarks.html#properties-summary">
+/// </summary>
+public class BookmarkWidget : Widget
+{
+    ///<inheritdoc/>
+    public override string WidgetType => "bookmark";
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Name { get; set; }
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Thumbnail { get; set; }
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? IconClass { get; set; }
+
+    [Parameter]
+    public new MapView? View { get; set; }
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Label { get; set; }
+
+}
+
