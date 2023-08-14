51a04736af4bc4dbca4cd7b3d81d73eecbf1bb30
Tim Rawson <rawson.t@gmail.com>
2023-08-14 12:34:20 -0700

    add imagerylayer class

---
+++ src/dymaptic.GeoBlazor.Core/Components/Layers/ImageryLayer.cs
@@ -0,0 +1,103 @@
+﻿using dymaptic.GeoBlazor.Core.Exceptions;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json.Serialization;
+
+
+namespace dymaptic.GeoBlazor.Core.Components.Layers;
+
+/// <summary>
+///     Represents a dynamic image service resource as a layer. An ImageryLayer retrieves and displays data from dynamic image services. An image service supports
+///     accessing the mosaicked image, its catalog, and the individual rasters in the catalog. An image service supports dynamic access and tiled access. 
+///     <a target="_blank" href="https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-ImageryLayer.html">
+///         ArcGIS
+///         JS API
+///     </a>
+/// </summary>
+public class ImageryLayer : Layer
+{
+    /// <inheritdoc />
+    [JsonPropertyName("type")]
+    public override string LayerType => "imagery";
+
+    /// <summary>
+    ///     Constructor for use as a razor component
+    /// </summary>
+    public ImageryLayer()
+    {
+    }
+
+    /// <summary>
+    ///     Constructor for use in code
+    /// </summary>
+    /// <param name="url">
+    ///     The url for the Imagery Layer source data.</param>    
+    /// /// <param name="portalItem">
+    ///     The portal item for the Imagery Layer source data.</param>
+
+    public ImageryLayer(string? url = null, PortalItem? portalItem = null)
+    {
+        if (url is null && portalItem is null)
+        {
+            throw new MissingRequiredOptionsChildElementException(nameof(KMLLayer),
+                new[] { nameof(Url), nameof(PortalItem) });
+        }
+        Url = url;
+        PortalItem = portalItem;
+    }
+
+    /// <summary>
+    ///     The url for the Imagery Layer source data.
+    /// </summary>
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [RequiredProperty(nameof(PortalItem))]
+    public string? Url { get; set; }
+
+    /// <summary>
+    ///     The portal item for the Imagery Layer source data.
+    /// </summary>
+    [RequiredProperty(nameof(Url))]
+    public PortalItem? PortalItem { get; set; }
+
+    /// <inheritdoc />
+    public override async Task RegisterChildComponent(MapComponent child)
+    {
+        switch (child)
+        {
+            case PortalItem portalItem:
+                if (!portalItem.Equals(PortalItem))
+                {
+                    PortalItem = portalItem;
+                    LayerChanged = true;
+                }
+
+                break;
+            default:
+                await base.RegisterChildComponent(child);
+
+                break;
+        }
+    }
+    /// <inheritdoc />
+    public override async Task UnregisterChildComponent(MapComponent child)
+    {
+        switch (child)
+        {
+            case PortalItem _:
+                PortalItem = null;
+                LayerChanged = true;
+
+                break;
+            default:
+                await base.UnregisterChildComponent(child);
+
+                break;
+        }
+    }
+    /// <inheritdoc />
+    internal override void ValidateRequiredChildren()
+    {
+        base.ValidateRequiredChildren();
+        PortalItem?.ValidateRequiredChildren();
+    }
+}
