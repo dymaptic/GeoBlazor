706456bc4f955cdb08a9138b524cffa6463af001
Tim Rawson <rawson.t@gmail.com>
2024-09-26 09:56:07 -0700

    working

---
+++ src/dymaptic.GeoBlazor.Core/Components/Widgets/AreaMeasurement2DWidget.cs
@@ -0,0 +1,32 @@
+﻿using dymaptic.GeoBlazor.Core.Objects;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Text.Json.Serialization;
+
+namespace dymaptic.GeoBlazor.Core.Components.Widgets;
+
+public class AreaMeasurement2DWidget : Widget
+{
+    [JsonPropertyName("type")]
+    public override string WidgetType => "areaMeasurement2D";
+
+    public DotNetObjectReference<AreaMeasurement2DWidget> AreaMeasurement2DWidgetObjectReference => DotNetObjectReference.Create(this);
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public ActiveTool? ActiveTool { get; set; }
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public AreaUnit? SystemOrAreaUnit { get; set; }
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Label { get; set; }
+
+    [Parameter]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public AreaUnit[]? AreaUnitOptions { get; set; }
+
+
+}
