c02421c45c7b1e32338ad3eaade796d7b735ddd3
Tim Purdum <tim.purdum@dymaptic.com>
2024-11-21 07:49:44 -0600

    fix serialization of TimeExtent

--- src/dymaptic.GeoBlazor.Core/Objects/Query.cs
+++ src/dymaptic.GeoBlazor.Core/Objects/Query.cs
@@ -1,5 +1,6 @@
 ﻿using dymaptic.GeoBlazor.Core.Components.Geometries;
 using dymaptic.GeoBlazor.Core.Serialization;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 
@@ -557,4 +558,57 @@ public enum OrderBy
 ///     A time extent for a temporal query against time-aware layers. For example, it can be used to discover all crimes
 ///     that occurred during the night shift from 10 PM to 6 AM on a particular date.
 /// </summary>
+[JsonConverter(typeof(TimeExtentConverter))]
 public record TimeExtent(DateTime Start, DateTime End);
+
+internal class TimeExtentConverter: JsonConverter<TimeExtent>
+{
+    public override TimeExtent Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        DateTime start = DateTime.MinValue;
+        DateTime end = DateTime.MinValue;
+        while (reader.TokenType != JsonTokenType.EndObject)
+        {
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                string? propertyName = reader.GetString();
+                reader.Read();
+                if (reader.TokenType == JsonTokenType.String)
+                {
+                    switch (propertyName)
+                    {
+                        case "start":
+                            start = DateTime.Parse(reader.GetString()!);
+                            break;
+                        case "end":
+                            end = DateTime.Parse(reader.GetString()!);
+                            break;
+                    }
+                }
+                else if (reader.TokenType == JsonTokenType.Number)
+                {
+                    switch (propertyName)
+                    {
+                        case "start":
+                            start = DateTimeOffset.FromUnixTimeMilliseconds(reader.GetInt64()).DateTime;
+                            break;
+                        case "end":
+                            end = DateTimeOffset.FromUnixTimeMilliseconds(reader.GetInt64()).DateTime;
+                            break;
+                    }
+                }
+            }
+            reader.Read();
+        }
+        return new TimeExtent(start, end);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TimeExtent value, JsonSerializerOptions options)
+    {
+        writer.WriteRawValue(JsonSerializer.Serialize(new
+        {
+            start = value.Start,
+            end = value.End
+        }));
+    }
+}
