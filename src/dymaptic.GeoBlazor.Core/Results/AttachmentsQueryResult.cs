dbb0e30bcebaa0c9dc60b8a469686ef51d3123af
Tim Purdum <tim.purdum@dymaptic.com>
2024-12-15 09:41:00 -0500

    rename attachmentsqueryresult

--- src/dymaptic.GeoBlazor.Core/Results/AttachmentsQueryResult.cs
+++ src/dymaptic.GeoBlazor.Core/Results/AttachmentsQueryResult.cs
@@ -6,4 +6,4 @@ namespace dymaptic.GeoBlazor.Core.Results;
 /// <summary>
 ///     Represents the result of a query for attachments, grouped by the source feature objectIds.
 /// </summary>
-public class AttachmentQueryResult: Dictionary<long, AttachmentInfo>;
+public class AttachmentsQueryResult: Dictionary<long, AttachmentInfo>;
