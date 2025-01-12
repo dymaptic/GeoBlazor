4a58674f48c0c4e941718568d22424ae3e57fcd5
TimPurdum <tim.purdum@dymaptic.com>
2025-01-12 07:53:30 -0600

    move pixelsize

--- src/dymaptic.GeoBlazor.Core/Model/PixelSize.cs
+++ src/dymaptic.GeoBlazor.Core/Model/PixelSize.cs
@@ -1,12 +1,11 @@
-namespace dymaptic.GeoBlazor.Core.Components;
-
+namespace dymaptic.GeoBlazor.Core.Model;
 /// <summary>
 ///     Raster pixel size.
 /// </summary>
-/// <param name="X">
+/// <param name = "X">
 ///     Pixel size along the X axis.
 /// </param>
-/// <param name="Y">
+/// <param name = "Y">
 ///     Pixel size along the Y axis.
 /// </param>
 public record PixelSize(double X, double Y);
