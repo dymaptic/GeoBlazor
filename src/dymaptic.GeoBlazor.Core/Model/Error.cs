d0ba921dc9cb0605d1bf95d7bc03990870eb4aae
Tim Purdum <tim.purdum@dymaptic.com>
2025-04-06 10:54:33 -0500

    reset changes

--- src/dymaptic.GeoBlazor.Core/Model/Error.cs
+++ src/dymaptic.GeoBlazor.Core/Model/Error.cs
@@ -1,15 +1,16 @@
-namespace dymaptic.GeoBlazor.Core.Enums;
+namespace dymaptic.GeoBlazor.Core.Model;
+
 /// <summary>
 ///     Error is a class that enhances the debugging and error handling process. Rather than returning a generic JavaScript error, this Error returns a standardized error object with several properties. The error class can be useful in many scenarios, such as working with promises, the esriRequest module, and many different layers and widgets.
 ///     <a target="_blank" href="https://developers.arcgis.com/javascript/latest/api-reference/esri-core-Error.html">ArcGIS JS API</a>
 /// </summary>
-/// <param name = "Name">
+/// <param name="Name">
 ///     A unique error name.
 /// </param>
-/// <param name = "Message">
+/// <param name="Message">
 ///     A message describing the details of the error.
 /// </param>
-/// <param name = "Details">
+/// <param name="Details">
 ///     The details object provides additional details specific to the error.
 /// </param>
 public record Error(string Name, string Message, object Details);
