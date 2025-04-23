2faf0c1d9dc9412472714fbd6e03894602196c4b
Tim Purdum <tim.purdum@dymaptic.com>
2025-04-22 21:57:20 -0500

    another run on scripts

--- src/dymaptic.GeoBlazor.Core/Scripts/geometryEngine.ts
+++ src/dymaptic.GeoBlazor.Core/Scripts/geometryEngine.ts
@@ -1,3 +1,4 @@
+import GeometryEngineGenerated from './geometryEngine.gb';
 import Geometry from "@arcgis/core/geometry/Geometry";
 import Polygon from "@arcgis/core/geometry/Polygon";
 import Polyline from "@arcgis/core/geometry/Polyline";
