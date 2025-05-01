34a879427e018a2a6eda51104f8303b4acc3a6c7
Tim Purdum <tim.purdum@dymaptic.com>
2025-04-30 19:40:32 -0500

    PR feedback fix.

--- src/dymaptic.GeoBlazor.Core/Scripts/sublayer.ts
+++ src/dymaptic.GeoBlazor.Core/Scripts/sublayer.ts
@@ -19,7 +19,7 @@ export async function buildJsSublayer(dotNetObject: any, layerId: string | null,
 export async function buildDotNetSublayer(jsObject: any): Promise<any> {
     let {buildDotNetSublayerGenerated} = await import('./sublayer.gb');
     let dnObject = await buildDotNetSublayerGenerated(jsObject);
-    if (hasValue(jsObject.sublayers) && jsObject.sublayers.length > 0) {
+    if (hasValue(jsObject.sublayers) && jsObject.sublayers.items.length > 0) {
         dnObject.sublayers = [];
         for (let i = 0; i < jsObject.sublayers.items.length; i++) {
             const sublayer = jsObject.sublayers.items[i];
