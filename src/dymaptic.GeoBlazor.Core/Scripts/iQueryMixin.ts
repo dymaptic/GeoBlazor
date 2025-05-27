5c96cf6f9ccc766f04ecc91a179c56d7703e012e
Tim Purdum <tim.purdum@dymaptic.com>
2025-05-27 17:40:50 -0500

    typescript cleanup

--- src/dymaptic.GeoBlazor.Core/Scripts/iQueryMixin.ts
+++ src/dymaptic.GeoBlazor.Core/Scripts/iQueryMixin.ts
@@ -1,10 +1,8 @@
 
 export async function buildJsIQueryMixin(dotNetObject: any): Promise<any> {
-    let { buildJsIQueryMixinGenerated } = await import('./iQueryMixin.gb');
-    return await buildJsIQueryMixinGenerated(dotNetObject);
+    // NOT NEEDED
 }     
 
 export async function buildDotNetIQueryMixin(jsObject: any): Promise<any> {
-    let { buildDotNetIQueryMixinGenerated } = await import('./iQueryMixin.gb');
-    return await buildDotNetIQueryMixinGenerated(jsObject);
+    // NOT NEEDED
 }
