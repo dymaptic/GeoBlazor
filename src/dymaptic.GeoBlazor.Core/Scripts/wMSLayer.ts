de5b81b5c84b360957c2ce93c254039b36d78b08
Ande Bell <ande.bell@dymaptic.com>
2025-07-16 12:36:14 -0600

    updating base test and typescript files

--- src/dymaptic.GeoBlazor.Core/Scripts/wMSLayer.ts
+++ src/dymaptic.GeoBlazor.Core/Scripts/wMSLayer.ts
@@ -1,7 +1,7 @@
 // override generated code in this file
 import WMSLayerGenerated from './wMSLayer.gb';
 import WMSLayer from '@arcgis/core/layers/WMSLayer';
-import {hasValue} from './arcGisJsInterop';
+import {arcGisObjectRefs, generateSerializableJson, hasValue} from './arcGisJsInterop';
 
 export default class WMSLayerWrapper extends WMSLayerGenerated {
 
@@ -12,6 +12,42 @@ export default class WMSLayerWrapper extends WMSLayerGenerated {
     async setSpatialReference(spatialReference: any): Promise<void> {
         let {buildJsSpatialReference} = await import('./spatialReference');
         this.layer.spatialReference = buildJsSpatialReference(spatialReference) as any;
+    }
+
+    async fetchImageAsString(extent: any,
+                             width: any,
+                             height: any,
+                             options: any): Promise<any> {
+        let {buildJsExtent} = await import('./extent');
+        let jsExtent = buildJsExtent(extent) as any;
+
+        let image = await this.layer.fetchImage(jsExtent,
+            width,
+            height,
+            options);
+
+        if (!hasValue(image)) {
+            return null;
+        }
+        return image.outerHTML;
+    }
+
+    async fetchImage(extent: any,
+                     width: any,
+                     height: any,
+                     options: any): Promise<any> {
+        let {buildJsExtent} = await import('./extent');
+        let jsExtent = buildJsExtent(extent) as any;
+
+        let image = await this.layer.fetchImage(jsExtent,
+            width,
+            height,
+            options);
+
+        if (!hasValue(image)) {
+            return null;
+        }
+        return await DotNet.createJSObjectReference(image);
     }
 }
 
@@ -35,11 +71,11 @@ export async function buildJsWMSLayer(dotNetObject: any, layerId: string | null,
                 // so this should always find a sublayer
                 jsSublayer = currentJsSublayers.find(s => s.name === sublayer.name);
             }
-            
+
             jsWmsLayer.sublayers.push(jsSublayer);
         }
     }
-    
+
     return jsWmsLayer;
 }
 
@@ -47,3 +83,5 @@ export async function buildDotNetWMSLayer(jsObject: any): Promise<any> {
     let {buildDotNetWMSLayerGenerated} = await import('./wMSLayer.gb');
     return await buildDotNetWMSLayerGenerated(jsObject);
 }
+
+
