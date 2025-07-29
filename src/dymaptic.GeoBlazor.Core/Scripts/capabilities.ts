50ddad34f7f8adbd6696d1cb5d3c2d8a1c973dcd
Tim Purdum <tim.purdum@dymaptic.com>
2025-07-28 22:48:17 -0500

    Simplify capabilities.ts

--- src/dymaptic.GeoBlazor.Core/Scripts/capabilities.ts
+++ src/dymaptic.GeoBlazor.Core/Scripts/capabilities.ts
@@ -1,10 +1,9 @@
+import {removeCircularReferences} from "./arcGisJsInterop";
 
-export async function buildJsCapabilities(dotNetObject: any, layerId: string | null, viewId: string | null): Promise<any> {
-    let { buildJsCapabilitiesGenerated } = await import('./capabilities.gb');
-    return await buildJsCapabilitiesGenerated(dotNetObject, layerId, viewId);
+export async function buildJsCapabilities(dotNetObject: any): Promise<any> {
+    // NOT USED, PLACEHOLDER
 }     
 
 export async function buildDotNetCapabilities(jsObject: any): Promise<any> {
-    let { buildDotNetCapabilitiesGenerated } = await import('./capabilities.gb');
-    return await buildDotNetCapabilitiesGenerated(jsObject);
+    return removeCircularReferences(jsObject);
 }
