d94e2cfabed86d038e4dfed5ef6f1e5b348cac6d
seahro <rawson.t@gmail.com>
2025-08-01 08:44:48 -0700

    working version - gets token and displays map

--- samples/dymaptic.GeoBlazor.Core.Sample.TokenRefresh/dymaptic.GeoBlazor.Core.Sample.TokenRefresh.Client/Program.cs
+++ samples/dymaptic.GeoBlazor.Core.Sample.TokenRefresh/dymaptic.GeoBlazor.Core.Sample.TokenRefresh.Client/Program.cs
@@ -1,5 +1,6 @@
 using dymaptic.GeoBlazor.Core;
 using dymaptic.GeoBlazor.Core.Model;
+using dymaptic.GeoBlazor.Core.Sample.TokenRefresh.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,9 +8,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Configuration.AddInMemoryCollection();
 builder.Services.AddGeoBlazor(builder.Configuration);
 
-HttpClient httpClient = new HttpClient();
-httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-//string token = await httpClient.GetStringAsync("api/auth/token");
-//string token = "AAPTBortrZkGbW0HtPXqP_T5jDQY3os1TbearzAwNA2BlQPvZHH62Z-nnJZ_kNwVdYjNWCc7OpmkkUA-QmVMBM0xr3NZ3tywQouHs0iFwlT8JjovXfpSJWCmxxlXpqJtfUQ76RnLuzPlv8hiKF96MRc8aieGovWN8RoayiDEkhNKzYssp0Pr81XKzJB-0-t0Q6OKlSfLwTAv8S3FCu_4W5y8XGO_qbD2HqpdtPatj_kRBpQ.";
+builder.Services.AddScoped<ArcGisAuthService>();
+builder.Services.AddScoped<HttpClient>(sp => 
+{
+    var httpClient = new HttpClient();
+    httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    return httpClient;
+});
 
 await builder.Build().RunAsync();
