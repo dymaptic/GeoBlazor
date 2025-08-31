5703116806455a5f744c8bc2cc2bc4784ab34f0a
Tim Purdum <tim.purdum@dymaptic.com>
2025-08-31 15:32:23 -0500

    fix clear locks

--- esBuildClearLocks.ps1
+++ esBuildClearLocks.ps1
@@ -1,5 +1,22 @@
-Remove-Item -Path (Join-Path $PSScriptRoot "src/dymaptic.GeoBlazor.Core/esBuild.Debug.lock") -Force
-Remove-Item -Path (Join-Path $PSScriptRoot "src/dymaptic.GeoBlazor.Core/esBuild.Release.lock") -Force
-Remove-Item -Path (Join-Path $PSScriptRoot "../src/dymaptic.GeoBlazor.Pro/esProBuild.Debug.lock") -Force
-Remove-Item -Path (Join-Path $PSScriptRoot "../src/dymaptic.GeoBlazor.Pro/esProBuild.Release.lock") -Force
+$CoreDebugLock = Join-Path $PSScriptRoot "src/dymaptic.GeoBlazor.Core/esBuild.Debug.lock"
+$CoreReleaseLock = Join-Path $PSScriptRoot "src/dymaptic.GeoBlazor.Core/esBuild.Release.lock"
+$ProDebugLock = Join-Path $PSScriptRoot "../src/dymaptic.GeoBlazor.Pro/esProBuild.Debug.lock"
+$ProReleaseLock = Join-Path $PSScriptRoot "../src/dymaptic.GeoBlazor.Pro/esProBuild.Release.lock"
+if (Test-Path $CoreDebugLock)
+{
+    Remove-Item -Path $CoreDebugLock -Force
+}
+if (Test-Path $CoreReleaseLock)
+{
+    Remove-Item -Path $CoreReleaseLock -Force
+}
+if (Test-Path $ProDebugLock)
+{
+    Remove-Item -Path $ProDebugLock -Force
+}
+if (Test-Path $ProReleaseLock)
+{
+    Remove-Item -Path $ProReleaseLock -Force
+}
+
 Write-Host "Cleared esBuild lock files"
