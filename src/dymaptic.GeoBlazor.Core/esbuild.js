515c8b6d56409be62f23250cd1d26449dbbe9aec
Tim Purdum <tim.purdum@dymaptic.com>
2025-10-03 10:42:32 -0500

    update error handling

--- src/dymaptic.GeoBlazor.Core/esbuild.js
+++ src/dymaptic.GeoBlazor.Core/esbuild.js
@@ -2,12 +2,12 @@ import esbuild from 'esbuild';
 import eslint from 'esbuild-plugin-eslint';
 import { cleanPlugin } from 'esbuild-clean-plugin';
 import path from 'path';
+import process from 'process';
 
 const args = process.argv.slice(2);
 const isDebug = args.includes('--debug');
 const isWatch = args.includes('--watch');
 const isRelease = args.includes('--release');
-
 const OUTPUT_DIR = path.resolve('./wwwroot/js');
 
 let options = {
@@ -30,5 +30,10 @@ if (isWatch) {
     let ctx = await esbuild.context(options);
     await ctx.watch();
 } else {
-    await esbuild.build(options);
+    try {
+        await esbuild.build(options);
+    } catch (err) {
+        console.error(`ESBuild Failed: ${err}`);
+        process.exit(1);
+    }
 }
