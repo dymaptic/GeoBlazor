b9964796708c4a3534b01fc5924933e0b073c29b
Tim Purdum <tim.purdum@dymaptic.com>
2025-10-27 17:20:31 -0500

    remove unused class

--- src/dymaptic.GeoBlazor.Core/Scripts/geoBlazorModuleLoader.ts
+++
@@ -1,6 +0,0 @@
-import AuthenticationManager from "./authenticationManager";
-import ProjectionWrapper from "./projection";
-import GeometryEngineWrapper from "./geometryEngine";
-import LocatorWrapper from "./locationService";
-import {loadProtobuf, ProtoGraphicCollection} from './geoBlazorCore';
-
